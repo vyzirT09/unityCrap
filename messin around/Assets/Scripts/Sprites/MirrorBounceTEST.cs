4312591d7939cfa0e04242bf15e889724b2aa9c0
Tucker <1623636@mukilteo.wednet.edu>
2018-11-08 10:24:51 -0800

    initial push

---
+++ messin around/Assets/Scripts/Sprites/MirrorBounceTEST.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorBounceTEST : MonoBehaviour
+{
+    private Vector3 v;
+
+	// Use this for initialization
+	void Start ()
+    {
+        v = new Vector3(2f, 0f);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position += v * Time.deltaTime;
+    }
+
+    void OnCollisionEnter(Collision col)   //MUST BE SPELLED PERFECTLY >_> ...
+    {
+        v = Vector3.Reflect(transform.right, col.transform.forward);
+    }
+}
